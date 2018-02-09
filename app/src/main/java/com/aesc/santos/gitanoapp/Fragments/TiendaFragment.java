ae139061efab71f569516177e3bc822267b7fea0
Erick1994Intecap <erickruiz1994@gmail.com>
2018-02-09 14:44:04 -0600

    Fragment Tienda

--- app/src/main/java/com/aesc/santos/gitanoapp/Fragments/TiendaFragment.java
+++ app/src/main/java/com/aesc/santos/gitanoapp/Fragments/TiendaFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,8 @@ import com.aesc.santos.gitanoapp.R;
  * create an instance of this fragment.
  */
 public class TiendaFragment extends Fragment {
+    private RecyclerView recyclerView;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
